26b416e9298c1296a3ea15c2783852fb9bf23503
kennell <kevin@fileperms.org>
2015-09-14 01:00:17 +0200

    VisualStudio 2015 compability

---
+++ Backup/PhraseApp/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace PhraseApp.PhraseApp
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidPhraseAppUpload = 0x100;
+        public const uint cmdidPhraseAppDownload = 0x200;
+
+    };
+}
