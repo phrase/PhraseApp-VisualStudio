bbf444f09971e1bd6e49ac2b1a0ef6caaf8bbf4b
kennell <kevin@fileperms.org>
2015-12-18 11:57:11 +0100

    initial new plugin

---
+++ PhraseApp/Cli.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PhraseApp
+{
+    class Cli
+    {
+        private String CliToolPath;
+        private String solutionDir;
+
+        public Cli(String CliToolPath, String solutionDir)
+        {
+            this.CliToolPath = CliToolPath;
+            this.solutionDir = solutionDir;
+        }
+
+        public Boolean ConfigFileExists()
+        {
+            //
+            return true;
+        }
+
+        public Boolean ClientValid()
+        {
+            return this.Info().Contains("PhraseApp");
+        }
+
+        public String Info()
+        {
+            return this.Exec("info");
+        }
+
+        public String Push()
+        {
+            return this.Exec("push");
+        }
+
+        public String Pull()
+        {
+            return this.Exec("pull");
+        }
+
+        public String Exec(String action)
+        {
+            String projectDir = this.solutionDir + "/" + Path.GetFileName(this.solutionDir);
+
+            Process process = new Process();
+            process.StartInfo.FileName = this.CliToolPath;
+            process.StartInfo.Arguments = action;
+            process.StartInfo.WorkingDirectory = projectDir;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            string err = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+            return output;
+        }
+
+    }
+}
---
+++ PhraseApp/PushCommand.cs
@@ -0,0 +1,112 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="PushCommand.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using EnvDTE;
+
+namespace PhraseApp
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class PushCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 4129;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("fccdbb59-737e-4dbc-a15b-f28e4118ed32");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PushCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private PushCommand(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static PushCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new PushCommand(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            DTE dte = (DTE)Package.GetGlobalService(typeof(DTE));
+            String solutionDir = System.IO.Path.GetDirectoryName(dte.Solution.FullName);
+            var opts = this.package.GetDialogPage(typeof(CliToolOtions)) as CliToolOtions;
+
+            Cli cli = new Cli(opts.CliToolPath, solutionDir);
+            String output = cli.Push();
+
+            var outputWindow = Package.GetGlobalService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+            var paneGuid = Microsoft.VisualStudio.VSConstants.OutputWindowPaneGuid.GeneralPane_guid;
+            IVsOutputWindowPane pane;
+            outputWindow.CreatePane(paneGuid, "PhraseApp", 1, 0);
+            outputWindow.GetPane(paneGuid, out pane);
+            pane.OutputString(output);
+        }
+    }
+}
